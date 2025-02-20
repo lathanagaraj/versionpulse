c28e86f1031d3ffcb4126fbe0cfd4fe5b48d5438
lathatridip <latha.tridip@gmail.com>
2025-02-19 23:03:21 -0500

    added worker pools

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+	"sync"
 	"time"
 	"versionpulse/vp"
 )
 
 const WebContentLenght int = 25000
+const maxWorkers = 1 // Limit the number of concurrent goroutines
 
 func main() {
 	start := time.Now()
@@ -22,31 +24,48 @@ func checkToolVersions() {
 		return
 	}
 
+	var wg sync.WaitGroup
+	var mu sync.Mutex // Mutex to synchronize access to toolVersions
+
 	var toolVersions []vp.ToolVersion
+
+	semaphore := make(chan struct{}, maxWorkers) // Buffered channel to limit concurrency
 
 	for _, tool := range tools.Tools {
 
-		siteContent, err := vp.NewScrapper(tool.URL, WebContentLenght).Scrape()
-		if err != nil {
-			log.Printf("Error fetching %s: %v", tool.URL, err)
-			continue
-		}
+		wg.Add(1)
+		semaphore <- struct{}{} // Acquire a slot
 
-		toolVersion, err := vp.NewChecker(tool.ID, tool.Name, siteContent).CheckVersion()
-		if err != nil {
-			log.Printf("Error extracting JSON object: %v", err)
-			continue
-		}
-		//time.Sleep(10 * time.Second)
+		go func(tool vp.Tool) {
+			defer wg.Done()
+			defer func() { <-semaphore }() // Release the slot
+			siteContent, err := vp.NewScrapper(tool.URL, WebContentLenght).Scrape()
+			if err != nil {
+				log.Printf("Error fetching %s: %v", tool.URL, err)
+				return
+			}
 
-		toolVersion.Link = tool.URL
-		toolVersions = append(toolVersions, *toolVersion)
+			toolVersion, err := vp.NewChecker(tool.ID, tool.Name, siteContent).CheckVersion()
+			if err != nil {
+				log.Printf("Error extracting JSON object: %v", err)
+				return
+			}
+			//time.Sleep(10 * time.Second)
 
-		log.Print("result " + toolVersion.Tool + " " + toolVersion.Version + " " + toolVersion.Date + " " + toolVersion.Link)
+			toolVersion.Link = tool.URL
+			mu.Lock()
+			toolVersions = append(toolVersions, *toolVersion)
+			mu.Unlock()
+
+			log.Print("result " + toolVersion.Tool + " " + toolVersion.Version + " " + toolVersion.Date + " " + toolVersion.Link)
+		}(tool)
 	}
 
+	wg.Wait()
 	if err := vp.NewToolsFeed(toolVersions).ToRss(); err != nil {
 		log.Fatalf("Error generating RSS feed: %v", err)
 	}
+
+	log.Println("All tools processed")
 
 }
